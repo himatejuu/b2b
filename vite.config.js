54627cc2fd3583e8ee72804238a91fa1b28334e6
I578457 <himateju.v@sap.com>
2026-06-09 15:33:19 +0530

    path fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/practice/',
+  base: '/b2b/',
   server: {
     port: 3000,
     open: true
